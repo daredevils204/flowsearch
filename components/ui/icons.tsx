ba758986212255b5b2b9120a4a68f6fa8d2d24e8
daredevils204 <dayn01914@gmail.com>
2025-03-04 15:12:55 +0700

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils'
 
 function IconLogo({ className, ...props }: React.ComponentProps<'h1'>) {
   return (
-    <h1 className="ml-2 font-bold tracking-tight text-xl">fl<span className="text-fuchsia-400">.</span></h1>
+    <h1 className="ml-2 font-bold tracking-tight text-lg">fl<span className="text-fuchsia-400">.</span></h1>
   )
 }
 
