033219e2a1f30b2148fb91c2744a2780ba2f4c1a
daredevils204 <dayn01914@gmail.com>
2025-03-04 13:29:00 +0700

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="35" height="35" viewBox="0 0 500.000000 500.000000"
+ width="30" height="30" viewBox="0 0 500.000000 500.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)"
